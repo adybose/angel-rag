e1b70ca1dbd14e718518e6a7216be7a083406229
Aditya Bose <adi26mar@gmail.com>
2025-06-07 21:18:25 +0530

    fix: change title and fix typo

--- frontend.py
+++ frontend.py
@@ -1,30 +1,10 @@
-# import streamlit as st
-# import requests
-
-# st.title("AngelOne Support Chatbot")
-# if "chat_history" not in st.session_state:
-#     st.session_state.chat_history = []
-
-# question = st.text_input("Ask a question:")
-# if st.button("Send"):
-#     response = requests.post(
-#         "https://angel-rag-production.up.railway.app/chat",
-#         json={"question": question, "chat_history": st.session_state.chat_history}
-#     ).json()
-#     if "answer" in response:
-#         st.write(f"**Answer**: \n{response['answer']}")
-#         st.session_state.chat_history = response["chat_history"]
-#     elif "detail" in response:
-#         st.error(f"Error: {response['detail']}")
-#     else:
-#         st.error("Unexpected response from server.")
 import streamlit as st
 import requests
 import json
 
 # Streamlit app configuration
-st.set_page_config(page_title="AbgelOne Support Chatbot", page_icon="🤖")
-st.title("RAG Chatbot")
+st.set_page_config(page_title="AngelOne Support Chatbot", page_icon="🤖")
+st.title("AngelOne Support Chatbot")
 
 # Initialize session state for chat history
 if "chat_history" not in st.session_state:
